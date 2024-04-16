85d760478c7865df3405751c01de3ff882db4b84
Farah Almas <syedalmasahmed@gmail.com>
2024-04-16 21:00:52 +0500

    first commit

---
+++ Project_3_todos/main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todosList = [];
+let condition = true;
+while (condition) {
+    let todosQues = await inquirer.prompt([
+        {
+            name: "firstQues",
+            type: "input",
+            message: "what would you like to add in your todos",
+        },
+        {
+            name: "secondQues",
+            type: "confirm",
+            message: "do you want to add more todos",
+            default: "true",
+        },
+    ]);
+    todosList.push(todosQues.firstQues);
+    console.log(todosList);
+    condition = todosQues.secondQues;
+}

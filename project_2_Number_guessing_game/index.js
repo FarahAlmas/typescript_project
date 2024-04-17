9fae910fa51bd757ac5656a956b081e4170d0005
Farah Almas <syedalmasahmed@gmail.com>
2024-04-17 13:05:59 +0500

    forth commit

---
+++ project_2_Number_guessing_game/index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let roundNumber = Math.floor((Math.random() * 4) + 1);
+let userInput = await inquirer.prompt({
+    type: "number",
+    name: "guessNumber",
+    message: "Enter a number between 1 to 4"
+});
+// -------conditional -------------
+if (userInput.guessNumber === roundNumber) {
+    console.log("congretulation! you guessed correctly ");
+}
+else {
+    console.log(`sorry! try next time. correct ans is: ${roundNumber}`);
+}
